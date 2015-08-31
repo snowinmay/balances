a0c934d776ae44777be05fa4d6cf1d10a3483ee6
prince <yujianqi@hsltech.com>
2015-08-31 18:18:51 +0800

    u

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var moment = require('moment');
+var nodeExcel = require('excel-export');
 
-mongoose.connect('mongodb://snowinmay:yujianqi2011@ds033133.mongolab.com:33133/prince-balances',function(){
-// mongoose.connect('mongodb://localhost/balances',function(){
+var pagination = {}
+var page_size = 10;
+
+mongoose.connect('mongodb://localhost/balances',function(){
 	console.log("Mongodb connected")
 })
 var Balance = mongoose.model('Balance', { 
@@ -12,6 +15,8 @@ var Balance = mongoose.model('Balance', {
 	price: Number, 
 	type: Boolean, 
 	from:String, 
+	// time: String,
+	time: { type: Date, default: Date.now()},
 	createdAt: { type: Date, default: Date.now()},
     updatedAt: { type: Date, default: Date.now()}
 });
@@ -23,14 +28,42 @@ router.get('/', function(req, res, next) {
 
 //index
 router.get('/balances', function(req, res, next) {
-	
-	Balance.find({},function(err,docs){
+	console.log(moment().format('YYYY-MM-DD HH:mm'))
+	pagination.current = parseInt(req.query.page) || 1;
+	Balance.count(function(err, count){
+		pagination.page_numbers = Math.ceil(count/page_size);
+		pagination.count = count;
+		pagination.start = parseInt(pagination.current)-2;
+		pagination.end = pagination.start + 4;
+		if (pagination.page_numbers <=5) {
+			pagination.start = 1;
+			pagination.end = pagination.page_numbers;
+		}else if (pagination.current <= 3) {
+			pagination.start = 1;
+			pagination.end = pagination.start + 4;
+		}else
+		if (pagination.page_numbers <= parseInt(pagination.current)+2) {
+			pagination.end = pagination.page_numbers;
+			pagination.start = pagination.page_numbers - 4;
+		}
+
+	})
+
+	var query = Balance.find({});
+	query.sort({time: -1});
+	query.limit(page_size);
+	query.skip((pagination.current-1)*page_size);
+
+	query.exec(function(err,docs){
+
 		res.render('balances/index', { 
 			title: '收支明细' ,
 			docs: docs,
+			pagination: pagination,
+			paginationJson:pagination,
 			moment: moment
 		});
-	})
+	});
 });
 
 //new
@@ -43,7 +76,8 @@ router.get('/balances/:id/edit', function(req, res, next) {
 	Balance.findById(req.params.id,function(err,doc){
 		res.render('balances/edit', {
 			title: '编辑收支明细',
-			doc:doc
+			doc:doc,
+			moment: moment
 		});
 	})
 });
@@ -51,8 +85,9 @@ router.get('/balances/:id/edit', function(req, res, next) {
 //create
 router.post('/balances', function(req, res, next) {	
 	req.body.createdAt = new Date();
+	console.log(typeof req.body.time)
 	var balance = new Balance(req.body);	
-	// console.log(balance)
+	console.log(balance)
 	balance.save(function (err) {
 	  if (!err){
 	  	res.redirect('/balances')
@@ -62,7 +97,7 @@ router.post('/balances', function(req, res, next) {
 	});
 });
 
-//update
+//update,delete
 router.all('/balances/:id', function(req, res, next) {
 	Balance.findById(req.params.id,function(err,doc){
 		if (req.body && req.body._method == 'put') {
@@ -101,18 +136,80 @@ router.all('/balances/:id', function(req, res, next) {
 	})
 });
 
-// 删除记录
-// var conditions = {username: 'emtity_demo_username'};
-// mongooseModel.remove(conditions, function(error){
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log('delete ok!');
-//     }
-//  
-//  
-//     //关闭数据库链接
-//     db.close();
-// });
+// export
+router.get('/export', function(req, res){
+    var conf ={};
+    conf.stylesXmlFile = "styles.xml";
+    conf.cols = [{
+        caption:'内容',
+        type:'string',
+        width:28.7109375
+    },
+    {
+        caption:'金额',
+         type:'number' 
+    },
+    {
+        caption:'类型',
+         type:'string' 
+    },
+    {    caption:'来源',
+         type:'string' 
+    },
+    {
+        caption:'日期',
+        type:'string'
+        // type:'date'
+        // beforeCellWrite:function(){
+        //     var originDate = new Date(Date.UTC(1899,11,30));
+        //     return function(row, cellData, eOpt){
+        //         if (eOpt.rowNum%2){
+        //             eOpt.styleIndex = 1;
+        //         }  
+        //         else{
+        //             eOpt.styleIndex = 2;
+        //         }
+        //         if (cellData === null){
+        //           	eOpt.cellType = 'string';
+        //          	return 'N/A';
+        //         } else
+        //         	console.log((cellData - originDate) / (24 * 60 * 60 * 1000))
+        //           	return (cellData - originDate) / (24 * 60 * 60 * 1000);
+        //     } 
+        // }()
+    }];
+    var page = parseInt(req.query.page) || 1;
+    console.log(req.query.page)
+    var arrAll =[];
+    var query = Balance.find({});
+	query.sort({time: -1});
+	query.limit(page_size);
+	query.skip((page-1)*page_size);
+	query.exec(function(err,docs){
+
+		for(var p in docs){
+			var arr =[];
+			arr.push(docs[p].content)
+			arr.push(docs[p].price)
+			arr.push(docs[p].type ? "收入" : "支出")
+			arr.push(docs[p].from)
+			arr.push(moment(docs[p].time).format('YYYY-MM-DD HH:mm'))
+			arrAll.push(arr)
+		}
+		conf.rows = arrAll;
+		var result = nodeExcel.execute(conf);
+	    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+	    res.setHeader("Content-Disposition", "attachment; filename=Page_"+page+"_Report.xlsx");
+	    res.end(result, 'binary');
+	});
+
+    // conf.rows = [
+    //     ["早饭",3.14,"支出","limi" ,new Date(Date.UTC(2013, 4, 1))],
+    //     ["午饭",2.7182,"支出","limi", new Date(2012, 4, 1)],
+    //     ["工资",10000,"收入","prince",new Date(Date.UTC(2013, 6, 9))],
+    //     ["话费",100,"支出","limi", null]  
+    // ];
+    
+});
 
 module.exports = router;
